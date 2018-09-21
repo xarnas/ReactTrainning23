9457e59a78b3325bc7b08d45e6b50c93bf9e2250
Arnas Petrauskas <a.petrauskas@kemek.eu>
2018-09-21 15:30:12 +0300

    Init

--- src/common/table.jsx
+++ src/common/table.jsx
@@ -3,6 +3,7 @@ import TableHeader from "./tableHeader";
 import TableBody from "./tableBody";
 
 const Table = ({ columns, sortColumn, onSort, data }) => {
+  console.log("test");
   return (
     <table className="table">
       <TableHeader columns={columns} sortColumn={sortColumn} onSort={onSort} />
